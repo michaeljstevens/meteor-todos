9d18dde95c25054a328596daf9db5514983cfd27
michaeljstevens <michaeljstevens@zoho.com>
2016-11-12 23:32:09 -0800

    added private and public tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -6,7 +6,12 @@ export const Tasks = new Mongo.Collection('tasks');
 
 if(Meteor.isServer) {
   Meteor.publish('tasks', function tasksPublication () {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
   });
 }
 
@@ -15,7 +20,7 @@ Meteor.methods({
     check(text, String);
 
     if(!this.userId) {
-      throw new Meteor.error('not-authorized');
+      throw new Meteor.Error('not-authorized');
     }
 
     Tasks.insert({
@@ -27,10 +32,34 @@ Meteor.methods({
   },
   'tasks.remove'(taskId) {
     check(taskId, String);
+    const task = Tasks.findOne(taskId);
+
+    if(task.private && task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
   'tasks.setChecked'(taskId, setChecked) {
     check(taskId, String);
+    const task = Tasks.findOne(taskId);
+
+    if(task.private && task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.update(taskId, { $set:{ checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    if(task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set:{ private: setToPrivate } });
   }
 });
